2091f72b74e7bd55b74550db8ec96915a6785aa9
suzzang <soochoi0325@gmail.com>
2019-08-08 18:42:54 +0900

    2743번 단어 길이 재기

---
+++ AlgorithmsPractice/src/P_2743.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class P_2743 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input_value = br.readLine();
+	
+		System.out.print(input_value.length());
+		
+		
+
+				
+	}
+
+}
